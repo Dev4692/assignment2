99c2ff6a5e363f14b8db8c14837e2f32b77e229b
Dev4692 <rhlrandive12@gmail.com>
2021-07-05 11:58:17 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component,OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit {
   title = 'assesment2';
+workout: any = {
+  name: '',
+};
+alloWAdd = false
+  rstbtn = false;
+  services = []
+  notes=  [];
+
+  constructor(){}
+  ngOnChanges(){  
+  }
+
+  ngOnInit(){
+  }
+
+  onServiceChanged(e) {
+    if (e == "" && this.services.length == 0) {
+      this.rstbtn = false;
+    } else {
+      var index = this.services.findIndex(x => x == e)
+      if (index <= -1) {
+        this.services.push(e);
+
+      }
+      else if (index > -1 && e != "") {
+        alert("Already Exist");
+      }
+      this.rstbtn = true;
+    }
+  }
+  onAddClick(inputtext){
+    this.notes.push(inputtext);
+    this.workout.name = ''
+  }
+
+  onResetTextClick(){
+    this.workout.name = ''
+  }
+
+  onResetClick(){
+    this.services.length=0;
+    this.rstbtn = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputsModule,
+    BrowserAnimationsModule,
+    DropDownsModule,
+    PopupModule,
+    TreeViewModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
